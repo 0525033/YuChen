6e70047209a5202991e2e1228171910a56fb5646
0525033 <46869238+0525033@users.noreply.github.com>
2020-02-22 02:11:05 +0800

    完成Cover、Resume的部分

---
+++ js.js
@@ -0,0 +1,20 @@
+function info(){}
+Info = new info();
+Info.addr="高雄市創新路68號";
+Info.scho=["高雄科技大學 行銷與流通管理系","北港高中 資料處理科"];
+Info.hist=["高雄扶青團 美宣長","高雄扶青團首敲典禮 司儀","行銷男排 隊長","Moocs LOGO設計競賽 第二名","Moocs LOGO設計競賽 最佳人氣獎","學生證生出來設計競賽 佳作"]
+Info.work=["台灣中油股份有限公司","2017-2020",["加油員","晚班班長"]]
+Info.work_career=["加油員","晚班班長"]
+
+function multi(tar,array,el){
+    for (i=0;i<array.length;i++){
+            $(tar).append("<"+el+">"+array[i]+"</"+el+">");
+    };
+};
+
+$(function(){
+    $('#addr').append(Info.addr);
+    multi('#scho',Info.scho,'h4');
+    multi('#hist',Info.hist,'h4');
+    multi('#work',Info.work,'h4')
+})
