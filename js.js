85e51c5d1c225ec4dffe82af1e1683180c0057c6
0525033 <46869238+0525033@users.noreply.github.com>
2020-02-23 20:54:25 +0800

    近期作品完成

--- js.js
+++ js.js
@@ -7,6 +7,13 @@ Info.work=["台灣中油股份有限公司","2017-2020",["加油員","晚班班�
 Info.work_career=["加油員","晚班班長"]
 
 Pages=['商品頁面','商品瀏覽','首頁','關於我們','購物車']
+Page_img=['img/Item.jpg','img/Products.jpg','img/Interface_index.jpg','img/About.jpg','img/Cart.jpg']
+Page_link=['https://0525033.github.io/chapei/Product_item.html','https://0525033.github.io/chapei/Products.html','https://0525033.github.io/chapei/','https://0525033.github.io/chapei/about.html','https://0525033.github.io/chapei/Cart.html']
+
+function img_link(img,link){
+    $('.card img').attr('src',img);
+    $('#chapei').attr('href',link);
+}
 
 function multi(tar,array,el){
     for (i=0;i<array.length;i++){
@@ -20,10 +27,39 @@ $(function(){
     multi('#hist',Info.hist,'h4');
     multi('#work',Info.work,'h4');
 
-    multi('#pages',Pages,'h6');
+    multi('#pages',Pages,"h6 class='title'");
+    $('h6').wrap("<a class='page' href='#'></a>");
     $('#pages h6').before("<div class='mark'></div>");
+    
+    $('.page:nth-child(3) .mark').addClass('mark-act');
 
-    $('.mark').click(function(){
-        $(this).toggleClass('mark-act')
+    $('.page:nth-child(1)').attr('href',Page_link[0]);
+    $('.page:nth-child(2)').attr('href',Page_link[1]);
+    $('.page:nth-child(3)').attr('href',Page_link[2]);
+    $('.page:nth-child(4)').attr('href',Page_link[3]);
+    $('.page:nth-child(5)').attr('href',Page_link[4]);
+
+    $('.page').hover(function(){
+        $('.mark').removeClass('mark-act');
+        $('h6').css('color','#AAA')
+        $('.mark',this).toggleClass('mark-act',1000,'easeOutSine');
+        $('h6',this).css('color','#DDD23B');
+        switch ($('h6',this).text()){
+            case '商品頁面':
+                img_link(Page_img[0],Page_link[0]);
+                break;
+            case '商品瀏覽':
+                img_link(Page_img[1],Page_link[1]);
+                break;
+            case '首頁':
+                img_link(Page_img[2],Page_link[2]);
+                break;
+            case '關於我們':
+                img_link(Page_img[3],Page_link[3]);
+                break;
+            case '購物車':
+                img_link(Page_img[4],Page_link[4]);
+                break;
+        }
     });
 })
